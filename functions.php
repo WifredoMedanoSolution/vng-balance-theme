26317b31ae87e7840c052256c68c12dee01b0794
Adrián Perera Hernández <adrian@medanosol.es>
2022-02-09 16:07:49 +0000

    Builds Aktuelles General and Single news

--- functions.php
+++ functions.php
@@ -10,21 +10,12 @@ show_admin_bar(false);
 
 function register_menu()
 {
-<<<<<<< HEAD
-    $locations = array(
-        'main' => 'Hauptnavigation',
-        'submenu' => 'Submenu',
-        'footer' => 'Footer Navigation',
-    );
-    register_nav_menus($locations);
-=======
 	$locations = array(
 		'main' => 'Hauptnavigation',
 		'submenu' => 'Submenu',
 		'footer' => 'Footer Navigation',
 	);
 	register_nav_menus($locations);
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
 }
 
 add_action('init', 'register_menu');
@@ -37,20 +28,6 @@ add_theme_support('post-thumbnails');
 add_image_size('asp', 734, 480, array('center', 'center'));
 
 add_action('wp_enqueue_scripts', function () {
-<<<<<<< HEAD
-    $hash = sha1_file(get_template_directory() . DIRECTORY_SEPARATOR . 'style.css');
-
-    wp_register_style('lightbox', get_template_directory_uri() . '/js/lib/lightbox/css/lightbox.min.css', array(), '2.9.0', 'all');
-    wp_enqueue_style('lightbox'); // Enqueue it!
-
-    wp_enqueue_style(
-        'theme-stylesheet',
-        get_template_directory_uri() . '/style.css',
-        [],
-        ($hash !== false ? substr($hash, -8, -1) : ''),
-        'all'
-    );
-=======
 	$hash = sha1_file(get_template_directory() . DIRECTORY_SEPARATOR . 'style.css');
 
 	wp_register_style('lightbox', get_template_directory_uri() . '/js/lib/lightbox/css/lightbox.min.css', array(), '2.9.0', 'all');
@@ -63,7 +40,6 @@ add_action('wp_enqueue_scripts', function () {
 		($hash !== false ? substr($hash, -8, -1) : ''),
 		'all'
 	);
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
 });
 
 /**
@@ -71,25 +47,11 @@ add_action('wp_enqueue_scripts', function () {
  */
 function enqueueStylesAndScripts()
 {
-<<<<<<< HEAD
-    /** Scripts * */
-    wp_register_script('jquery', get_template_directory_uri() . '/js/lib/jquery-1.12.4.min.js');
-    wp_register_script('isotope', get_template_directory_uri() . '/js/lib/jquery.isotope-3.0.4.min.js', 'jquery');
-    wp_register_script('lightbox', get_template_directory_uri() . '/js/lib/lightbox/js/lightbox.min.js', 'jquery');
-    wp_register_script('action', get_template_directory_uri() . '/js/scripts.js', 'jquery');
-
-
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('isotope');
-    wp_enqueue_script('lightbox');
-    wp_enqueue_script('action');
-=======
 	/** Scripts * */
 	wp_register_script('jquery', get_template_directory_uri() . '/js/lib/jquery-1.12.4.min.js');
 	wp_register_script('isotope', get_template_directory_uri() . '/js/lib/jquery.isotope-3.0.4.min.js', 'jquery');
 	wp_register_script('lightbox', get_template_directory_uri() . '/js/lib/lightbox/js/lightbox.min.js', 'jquery');
 	wp_register_script('action', get_template_directory_uri() . '/js/scripts.js', 'jquery');
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
 
     // wp_register_script('mapbox_action', get_template_directory_uri() . '/js/mapbox_action.js', 'jquery', '1.0', false);
     // wp_enqueue_script('mapbox_action');
@@ -109,11 +71,7 @@ add_action('get_header', 'my_filter_head');
 
 function my_filter_head()
 {
-<<<<<<< HEAD
-    remove_action('wp_head', '_admin_bar_bump_cb');
-=======
 	remove_action('wp_head', '_admin_bar_bump_cb');
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
 }
 
 function my_mce4_options($init)
@@ -140,24 +98,6 @@ add_filter('tiny_mce_before_init', 'my_mce4_options');
  * VC Templates
  */
 if (is_plugin_active('js_composer/js_composer.php')) {
-<<<<<<< HEAD
-    vc_set_shortcodes_templates_dir(
-        get_template_directory()
-            . DIRECTORY_SEPARATOR . 'js_composer'
-            . DIRECTORY_SEPARATOR . 'templates'
-            . DIRECTORY_SEPARATOR . 'shortcodes'
-    );
-    include_once 'vc_templates/vc_yellowbox.php';
-    include_once 'vc_templates/vc_asp.php';
-    include_once 'vc_templates/vc_pagemenu.php';
-    include_once 'vc_templates/vc_referenz_acc.php';
-    include_once 'vc_templates/vc_totop.php';
-    include_once 'vc_templates/vc_acc.php';
-    include_once 'vc_templates/vc_mapitem.php';
-    include_once 'vc_templates/vc_header.php';
-    include_once 'vc_templates/vc_gallery.php';
-    include_once 'vc_templates/vc_mapbox.php';
-=======
 	vc_set_shortcodes_templates_dir(
 		get_template_directory()
 			. DIRECTORY_SEPARATOR . 'js_composer'
@@ -173,7 +113,6 @@ if (is_plugin_active('js_composer/js_composer.php')) {
 	include_once 'vc_templates/vc_mapitem.php';
 	include_once 'vc_templates/vc_header.php';
 	include_once 'vc_templates/vc_gallery.php';
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
 }
 
 /**
@@ -181,13 +120,8 @@ if (is_plugin_active('js_composer/js_composer.php')) {
  */
 function kb_svg($svg_mime)
 {
-<<<<<<< HEAD
-    $svg_mime['svg'] = 'image/svg+xml';
-    return $svg_mime;
-=======
 	$svg_mime['svg'] = 'image/svg+xml';
 	return $svg_mime;
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
 }
 
 add_filter('upload_mimes', 'kb_svg');
@@ -217,93 +151,6 @@ add_filter('wp_check_filetype_and_ext', 'kb_ignore_upload_ext', 10, 4);
 class WPBakeryShortCode_zweiklick_youtube_video extends WPBakeryShortCode
 {
 
-<<<<<<< HEAD
-    protected function content($atts, $content = null)
-    {
-        extract(shortcode_atts(array(
-            'video_url' => '',
-            'container_height' => '400',
-            'dsgvo_img' => '4673',
-        ), $atts));
-
-        parse_str(parse_url($video_url, PHP_URL_QUERY), $my_array_of_vars);
-        $video_id = $my_array_of_vars['v'];
-
-        $output = '<div class="video_wrapper" style="height:' . $container_height . 'px; background-image: url(' . wp_get_attachment_image_src($dsgvo_img, 'full')[0] . ');">';
-        $output .= '<div class="video_trigger" data-source="' . $video_id . '">';
-        $output .= '<p class="text-center">';
-        $output .= wpb_js_remove_wpautop($content, true);
-        $output .= '</p>';
-        $output .= '<div class="button-container">';
-        $output .= '<input type="button" class="button-inner" value="Akzeptieren" />';
-        $output .= '</div>';
-        $output .= '</div>';
-        $output .= '<div class="video_layer"><iframe src="" border="0"></iframe></div>';
-        $output .= '</div>';
-
-
-        return $output;
-    }
-}
-
-vc_map(array(
-    'base' => 'zweiklick_youtube_video',
-    'name' => 'Zwei-Klick Youtube Video',
-    'content_element' => true,
-    'icon' => 'zweiklick_youtube_video',
-    "show_settings_on_create" => true,
-    "params" => array(
-        array(
-            "type" => "textfield",
-            "heading" => "Video URL",
-            "param_name" => "video_url",
-            "holder" => "h1",
-        ),
-        array(
-            "type" => "textfield",
-            "heading" => "Höhe",
-            "param_name" => "container_height",
-            "holder" => "div",
-        ),
-        array(
-            "type" => "textarea_html",
-            "heading" => "Hinweistext",
-            "param_name" => "content",
-            "holder" => "div",
-            "value" => "Zum Abspielen dieses YouTube-Videos auf unserer Webseite müssen Sie zustimmen. Wir möchten Sie darauf hinweisen, dass durch den Start des YouTube-Videos Daten an YouTube übermittelt werden. Weitere Informationen erhalten Sie in unsere <a href='/datenschutz' target='_blank'>Datenschutzerklärung</a>",
-        ),
-        array(
-            "type" => "attach_image",
-            "heading" => "Placeholder Image",
-            "param_name" => "dsgvo_img",
-            "holder" => "img",
-        )
-    )
-));
-
-// If Dynamic Sidebar Exists
-function sidebar()
-{    // Define Sidebar Widget Area 1
-    register_sidebar(
-        array(
-            'name' => __('Language-Switcher', 'html5blank'),
-            'description' => __('Language-Switcher', 'html5blank'),
-            'id' => 'nav-language-switcher',
-            'before_widget' => '<div id="%1$s" class="%2$s">',
-            'after_widget' => '</div>',
-            'before_title' => '<h3>',
-            'after_title' => '</h3>'
-        )
-    );
-}
-
-add_action('widgets_init', 'sidebar');
-
-
-
-require_once 'lib/NewsPostType.php';
-new NewsPostType();
-=======
 	protected function content($atts, $content = null)
 	{
 		extract(shortcode_atts(array(
@@ -366,4 +213,3 @@ vc_map(array(
 		)
 	)
 ));
->>>>>>> 46b139dc8345d814304fce5dc4fbf9d0f4ff0271
--- single-news.php
+++ single-news.php
@@ -1,71 +1,80 @@
-<?php get_header(); ?>
+<?php get_header();?>
 
 <div class="spinner-wrapper">
     <div class="spinner"></div>
 </div>
 <main role="main" class="single-news">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <!-- <?php if ( have_posts() ): while ( have_posts() ): the_post();?>
 
-    <?php if (!empty($header_image = get_post_meta(get_the_ID(), 'header-image', true))) : ?>
-    <div class="single-news-header header-image"
-        style="background-image: url(<?php echo wp_get_attachment_url($header_image); ?>)">
-    </div>
-    <?php else : ?>
-    <div class="single-news-header yellow">
-        <div class="vc_container">
-            <div class="vc_row">
-                <div class="wpb_column vc_column_container vc_col-sm-12">
-                    <div class="vc_column-inner">
-                        <div class="wpb_wrapper">
-                            <div class="wpb_text_column wpb_content_element ">
-                                <div class="wpb_wrapper">
-                                    <h1>Aktuelles</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php endif; ?>
+																    <?php if ( ! empty( $header_image = get_post_meta( get_the_ID(), 'header-image', true ) ) ): ?>
+																    <div class="single-news-header header-image"
+																        style="background-image: url(<?php echo wp_get_attachment_url( $header_image ); ?>)">
+																    </div>
+																    <?php else: ?> -->
+								    <!-- <div class="single-news-header yellow">
+								        <div class="vc_container">
+								            <div class="vc_row">
+								                <div class="wpb_column vc_column_container vc_col-sm-12">
+								                    <div class="vc_column-inner">
+								                        <div class="wpb_wrapper">
+								                            <div class="wpb_text_column wpb_content_element ">
+								                                <div class="wpb_wrapper">
+								                                    <h1>Aktuelles</h1>
+								                                </div>
+								                            </div>
+								                        </div>
+								                    </div>
+								                </div>
+								            </div>
+								        </div>
+								    </div> -->
+								    <!-- <?php endif;?> -->
     <div class="vc_container">
         <div class="vc_row-full-width vc_clearfix"></div>
         <div class="vc_row wpb_row vc_row-fluid">
             <div class="wpb_column vc_column_container vc_col-sm-12">
                 <div class="vc_column-inner">
                     <div class="wpb_wrapper">
-                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <article id="post-<?php the_ID();?>" <?php post_class();?>>
+							<div class="single-news-wrapper">
+								<div class="single-news-details">
+									<div class="single-news-date">
+										<i class="fas fa-circle"></i>
+										<?php the_date();?>
+									</div>
+									<div class="single-news-category">
+										<?php the_category( ', ' );?>
+									</div>
+								</div>
 
-                            <div class="single-news-title">
-                                <h1><?php the_title(); ?></h1>
-                            </div>
+								<div class="single-news-title">
+									<h1><?php the_title();?></h1>
+								</div>
 
-                            <div class="single-news-thumbnail">
-                                <?php the_post_thumbnail('medium'); ?>
-                            </div>
+								<div class="single-news-thumbnail">
+									<?php the_post_thumbnail( 'full' );?>
+								</div>
+								<div class="single-news-content">
+									<?php the_content();?>
+								</div>
+								<?php if ( ! empty( $pdf = get_post_meta( get_the_ID(), 'pdf-news', true ) ) ): ?>
+									<div class="single-news-pdf balance-download vc_btn3-container vc_btn3-left">
+										<a class="bal_download-button vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-round vc_btn3-style-custom" target="_blank" href="<?php echo wp_get_attachment_url( $pdf ) ?>">
+										DOWNLOAD <?php echo strtoupper( get_the_title( $pdf ) ) ?></a>
+									</div>
+									<?php endif;?>
 
-                            <div class="single-news-date">
-                                <?php the_date(); ?>
-                                <?php if (!empty($pdf = get_post_meta(get_the_ID(), 'pdf-news', true))) : ?>
-                                <div class="single-news-pdf">
-                                    <a target="_blank" href="<?php echo wp_get_attachment_url($pdf) ?>">
-                                        <img src="/wp-content/themes/balance/img/application-pdf.png" />
-                                    </a>
-                                </div>
-                                <?php endif; ?>
-                            </div>
-                            <div class="single-news-content">
-                                <?php the_content(); ?>
-                            </div>
+							</div>
                         </article>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <?php endwhile; ?>
-    <?php endif; ?>
+    <?php endwhile;?>
+    <?php endif;?>
 </main>
 
-<?php get_footer(); ?>
+<?php get_footer();?>
+<link rel="stylesheet" id="vc_font_awesome_5_shims-css" href="https://bal.vng.aliado.rocks/wp-content/plugins/js_composer/assets/lib/bower/font-awesome/css/v4-shims.min.css?ver=6.2.0" type="text/css" media="all">
+<link rel="stylesheet" id="vc_font_awesome_5-css" href="https://bal.vng.aliado.rocks/wp-content/plugins/js_composer/assets/lib/bower/font-awesome/css/all.min.css?ver=6.2.0" type="text/css" media="all">
